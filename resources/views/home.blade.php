1f7bf5f37231484e60ef7e6f5e333d5953d2044f
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-12 17:22:09 +0200

    printing in a better style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
     @foreach ($trains as $train)
-        <div>
-            {{ $train }}
+        <div class="container d-flex">
+            <div>{{ $train->agency }}-</div>
+            <div>{{ $train->leaving_station }}-</div>
+            <div>{{ $train->arrival_station }}-</div>
+            <div>{{ $train->leaving_time }}-</div>
+            <div>{{ $train->arrival_time }}-</div>
+            <div>{{ $train->train_code }}-</div>
+            <div>{{ $train->wagon_number }}-</div>
+            <div>{{ $train->on_time }}-</div>
+            <div>{{ $train->cancelled }}-</div>
+            <div>{{ $train->train_date }}</div>
         </div>
     @endforeach
 @endsection
